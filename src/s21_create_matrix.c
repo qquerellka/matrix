0ca3cfe99ac11ef146387607f5e8a1d8e014c37c
qquerellka <uskovmaksof@gmail.com>
2025-04-07 13:37:41 +0300

    init project

---
+++ src/s21_create_matrix.c
@@ -0,0 +1,9 @@
+// #include "s21_matrix.h"
+
+// int s21_create_matrix(int rows, int columns, matrix_t *result) {
+//   for ( int i = 0; i < rows; i++) {
+//     for (int j = 0; j < columns; j++) {
+//       result[i = 
+//     }
+//   }
+// }
