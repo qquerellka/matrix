bdda139c3f7a8baab94e62ce2fc19029ee84de30
qquerellka <uskovmaksof@gmail.com>
2025-04-29 15:31:08 +0300

    create s21_sum & s21_sub

---
+++ src/s21_sub_matrix.c
@@ -0,0 +1,25 @@
+#include "s21_matrix.h"
+
+
+
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  if (A == NULL || B == NULL || A->matrix == NULL || B->matrix == NULL) {
+    return INCORRECT_MATRIX;
+  }
+
+  if (A->rows != B->rows || A->columns != B->columns) {
+    return INCORRECT_COUNT;
+  }
+
+  int code = s21_create_matrix(A->rows, A->columns, result);
+  if (code != OK) {
+    return code;
+  }
+
+  for (int i = 0; i < A->rows; i++) {
+    for (int j = 0; j < A->columns; j++) {
+      result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+    }
+  }
+  return OK;
+}
