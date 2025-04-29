cd6fea8b71a49f63864dc20aed4af751e74354eb
qquerellka <uskovmaksof@gmail.com>
2025-04-29 11:24:15 +0300

    create base functions: sreate&remove

--- src/s21_create_matrix.c
+++ src/s21_create_matrix.c
@@ -1,9 +1,28 @@
-// #include "s21_matrix.h"
+#include "s21_matrix.h"
 
-// int s21_create_matrix(int rows, int columns, matrix_t *result) {
-//   for ( int i = 0; i < rows; i++) {
-//     for (int j = 0; j < columns; j++) {
-//       result[i = 
-//     }
-//   }
-// }
+int s21_create_matrix(int rows, int columns, matrix_t *result) {
+  if (rows <= 0 || columns <= 0 || result == NULL) {
+    return INCORRECT_MATRIX;
+  }
+
+  result->rows = rows;
+  result->columns = columns;
+
+  // Выделяем память под массив указателей + под данные
+  result->matrix = (double **)malloc(rows * sizeof(double *));
+  if (result->matrix == NULL) return INCORRECT_MATRIX;
+
+  double *data = (double *)calloc(rows * columns, sizeof(double));
+  if (data == NULL) {
+    free(result->matrix);
+    result->matrix = NULL;
+    return INCORRECT_MATRIX;
+  }
+
+  // Привязываем строки к нужным позициям в общем блоке
+  for (int i = 0; i < rows; i++) {
+    result->matrix[i] = data + i * columns;
+  }
+
+  return OK;
+}
---
+++ src/s21_remove_matrix.c
@@ -0,0 +1,13 @@
+#include "s21_matrix.h"
+
+void s21_remove_matrix(matrix_t *A) {
+  if (A != NULL && A->matrix != NULL) {
+    if (A->matrix[0] != NULL) {
+      free(A->matrix[0]);  // Сначала освобождаем блок данных
+    }
+    free(A->matrix);  // Потом массив указателей на строки
+    A->matrix = NULL;
+    A->rows = 0;
+    A->columns = 0;
+  }
+}
